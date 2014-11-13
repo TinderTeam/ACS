437f20f3e033aa52679c4c848ba14352fe3d87d9
南博文 <nanbowen@fuego.cn>
2014-11-13 15:50:07 +0800

    Signed-off-by: 南博æ? <nanbowen@fuego.cn>

---
+++ code/ACS/Common/Service/DataManipulationAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACS.Common.Service
+{
+    public interface DataManipulationAdapter
+    {
+        bool adatptor<T>(T po);
+    }
+
+}
---
+++ code/ACS/Common/Service/impl/DataManipulationServiceImpl.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ACS.Common.Constant;
+using ACS.Common.Dao;
+using ACS.Common.Dao.impl;
+using ACS.Common.Service.exception;
+using ACS.Models.Po.CF;
+
+namespace ACS.Common.Service.impl
+{
+    public class DataManipulationServiceImpl<T>:DataManipulationService<T>
+    {
+        DataManipulationService<User> service = new DataManipulationServiceImpl<User>();
+        private DataManipulationAdapter addAdapter;
+
+        CommonDao<T> dao = new CommonDaoImpl<T>();
+
+        DataManipulationServiceImpl()
+        {
+            addAdapter = new EmptyDataManipulationAdapter();
+        }
+        public void addAddAdaptor(DataManipulationAdapter a)
+        {
+            addAdapter = a;
+        }
+
+
+        public void add(T po)
+        {
+            if (addAdapter.adatptor<T>(po))
+            {
+                try
+                {
+
+                    dao.create(po);
+                }
+                catch (Exception ex)
+                {                    
+                    throw new Exception(DataManipulationException.AddExceptionMsg);
+                }
+            }
+            else
+            {
+                throw new Exception(DataManipulationException.VerificationExceptionMsg);
+            }
+        }
+
+        public void delete(String attr,String value)
+        {         
+            try
+            {
+                dao.delete(new QueryCondition(
+                    ConditionTypeEnum.EQUAL,
+                    attr,
+                    value
+                    ));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(DataManipulationException.deleteExceptionMsg);
+            } 
+         
+        }
+
+        public void modify(T obj)
+        {
+            try
+            {
+                dao.update(obj);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(DataManipulationException.updateExceptionMsg);
+            } 
+        }
+    }
+}
